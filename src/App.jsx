64f3012c6f33b6e70a2fa2270a5c0eae4f80a305
0rogerinho <luaansilva11@gmail.com>
2023-08-19 15:51:23 -0300

    atualização

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import AdvancedStatistics from './AdvancedStatistics/AdvancedStatistics';
-import Nav from './Header/nav';
-import Hero from './Hero/Hero';
-import SendUrl from './SendUrl/SendUrl';
+import Nav from '../src/Header/Nav';
+import Hero from '../src/Hero/Hero';
+import SendUrl from '../src/SendUrl/SendUrl';
 
 function App() {
   return (
