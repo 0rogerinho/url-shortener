c1c00d144b0f2bb184d70b332957ff17cd861072
0rogerinho <luaansilva11@gmail.com>
2023-08-19 16:01:00 -0300

    teste

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import AdvancedStatistics from './AdvancedStatistics/AdvancedStatistics';
-import Nav from '../src/Header/Nav';
-import Hero from '../src/Hero/Hero';
-import SendUrl from '../src/SendUrl/SendUrl';
+import Nav from './Header/nav';
+import Hero from './Hero/Hero';
+import SendUrl from './SendUrl/SendUrl';
 
 function App() {
   return (
